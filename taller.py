bd50611248f7fa5a9b8d3d70572929f62947fa71
Diego9705 <115195973+Diego9705@users.noreply.github.com>
2024-03-10 16:04:25 -0500

    Update taller.py
    Se metió un interface de movimiento y una nueva clase de campeón como ejemplo

--- taller.py
+++ taller.py
@@ -1,79 +1,106 @@
+class IMovimiento:
+    def tipo_movimiento(self):
+        pass
+
+class velocidad_baja(IMovimiento):
+
+    def tipo_movimiento(self):
+        print("Se mueve 1 unidad")
+
+class velocidad_media(IMovimiento):
+
+    def tipo_movimiento(self):
+        print("Se mueve 2 unidades")
+
+class velocidad_alta(IMovimiento):
+
+    def tipo_movimiento(self):
+        print("Se mueve 3 unidades")
+
+
 class IAtaque:
     def tipo_ataque(self):
         pass
-    def movimiento(self):
-        pass
-
+   
 class espada_ligera(IAtaque):
     def tipo_ataque(self):
         print("Atacando rapido")
-    
-    def movimiento(self):
-        print("Se mueve 3 unidades")
 
 class espada_pesada(IAtaque):
     def tipo_ataque(self):
         print("Atacando lento pero eficaz")
 
-    def movimiento(self):
-        print("Se mueve 1 unidad")
-
 class arco(IAtaque):
     def tipo_ataque(self):
         print("Atacando a distancia")
 
-    def movimiento(self):
-        print("Se mueve 2 unidades")
-
-
 class Campeon:
 
-    def __init__(self,ataque:IAtaque):
+    def __init__(self,ataque:IAtaque,movimiento:IMovimiento):
         self.ataque = ataque
+        self.movimiento = movimiento
     
     def perform_ataque(self):
         self.ataque.tipo_ataque()
 
     def perform_movimiento(self):
-        self.ataque.movimiento()
+        self.movimiento.tipo_movimiento()
 
     def discurso(self):
         print("Por demacia")
 
 class Garen_caballero(Campeon):
     def __init__(self):
-        super().__init__(espada_pesada())
-
+        super().__init__(espada_pesada(),velocidad_media())
+        
     def discurso(self):
-        print("POOR DEMACIAA")
+        print("Garen reportandose, POOOOR DEMACIAA")
 
     
 
 class Fiora_soldado(Campeon):
     def __init__(self):
-        super().__init__(espada_ligera())
+        super().__init__(espada_ligera(),velocidad_alta())
+
 
     def discurso(self):
-        print("Porrgg demaciaa")
+        print("Fiora reportandose, Porrgg demaciaa")
 
 class Vayne_arquera(Campeon):
     def __init__(self):
-        super().__init__(arco())
+        super().__init__(arco(),velocidad_media())
+  
 
     def discurso(self):
-        print("Entre las sombras")       
+        print("Vayne reportandose, entre las sombras")  
+
+class Aatrox_caballero(Campeon):
+    def __init__(self):
+        super().__init__(espada_pesada(),velocidad_baja())
+    
+
+    def discurso(self):
+        print("Soy el destructor de mundos")      
     
 
 if "__main__" == __name__:
 
     garen1 = Garen_caballero()
+    garen1.discurso()
     garen1.perform_ataque()
     garen1.perform_movimiento()
-
+    print("\n")
     fiora1 = Fiora_soldado()
+    fiora1.discurso()
     fiora1.perform_ataque()
     fiora1.perform_movimiento()
-
+    print("\n")
     vayne1 = Vayne_arquera()
+    vayne1.discurso()
     vayne1.perform_ataque()
     vayne1.perform_movimiento()
+    print("\n")
+    aatrox1= Aatrox_caballero()
+    aatrox1.discurso()
+    aatrox1.perform_ataque()
+    aatrox1.perform_movimiento()
